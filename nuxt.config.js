5b61b6f718815a831735b1bee812f97ed17d8a67
Mark Sonnenschein <marksonn5@gmail.com>
2020-04-11 16:08:23 +1000

    Changed color scheme to check if pipeline works

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.green.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
